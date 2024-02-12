4c480134b66b45d8bd846638c0410ec911fa10e6
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-02-12 23:32:55 +0300

    refactor

--- src/server.ts
+++ src/server.ts
@@ -47,82 +47,81 @@ export const initServer = (): Server => {
       res.end(JSON.stringify(body));
     };
 
-    server.on('error', () => {
-      handleServerError(sendResponse);
-    });
-
     if (!url || !method) {
       handleNotFound(sendResponse, strings.errors.urlOrMethodNotFound);
       return;
     }
 
     const parsedUrl = new URL(url, `http://${req.headers.host}`).pathname;
-    //get rid of //// slashes in the end and normalize it to only one
+
     const normalizedUrl = normalizeUrl(parsedUrl);
     const userId = getUserId(normalizedUrl);
+    try {
+      switch (normalizedUrl) {
+        case BASE_ENDPOINT:
+          if (method === HttpMethod.GET) {
+            handleGetUsers(sendResponse);
+          } else if (method === HttpMethod.POST) {
+            req.on('data', (chunk) => {
+              const userData = getUserData(chunk);
 
-    switch (normalizedUrl) {
-      case BASE_ENDPOINT:
-        if (method === HttpMethod.GET) {
-          handleGetUsers(sendResponse);
-        } else if (method === HttpMethod.POST) {
-          req.on('data', (chunk) => {
-            const userData = getUserData(chunk);
+              if (!userData) {
+                handleBadRequest(sendResponse, strings.errors.invalidJSON);
+                return;
+              }
 
-            if (!userData) {
-              handleBadRequest(sendResponse, strings.errors.invalidJSON);
-              return;
-            }
+              handlePostUser(sendResponse, userData);
+            });
+          } else {
+            handleInvalidMethod(sendResponse);
+          }
+          break;
+        case `${BASE_ENDPOINT}/${userId}`:
+          if (!resourceIdMethods.includes(method)) {
+            handleInvalidMethod(sendResponse);
+            return;
+          }
 
-            handlePostUser(sendResponse, userData);
-          });
-        } else {
-          handleInvalidMethod(sendResponse);
-        }
-        break;
-      case `${BASE_ENDPOINT}/${userId}`:
-        if (!resourceIdMethods.includes(method)) {
-          handleInvalidMethod(sendResponse);
-          return;
-        }
+          const user = users.find((user) => user.id === userId);
 
-        const user = users.find((user) => user.id === userId);
+          if (!userId) {
+            return;
+          }
 
-        if (!userId) {
-          return;
-        }
+          if (!uuidRegex.test(userId)) {
+            handleBadRequest(sendResponse, strings.errors.invalidUserId);
+            return;
+          }
+          if (!user) {
+            handleNotFound(sendResponse, strings.errors.noSuchUser);
+            return;
+          }
 
-        if (!uuidRegex.test(userId)) {
-          handleBadRequest(sendResponse, strings.errors.invalidUserId);
-          return;
-        }
-        if (!user) {
-          handleNotFound(sendResponse, strings.errors.noSuchUser);
-          return;
-        }
+          if (method === HttpMethod.GET) {
+            handleGetUser(sendResponse, user);
+          } else if (method === HttpMethod.PUT) {
+            req.on('data', (chunk) => {
+              const userData = getUserData(chunk);
 
-        if (method === HttpMethod.GET) {
-          handleGetUser(sendResponse, user);
-        } else if (method === HttpMethod.PUT) {
-          req.on('data', (chunk) => {
-            const userData = getUserData(chunk);
+              if (!userData) {
+                handleBadRequest(sendResponse, strings.errors.invalidJSON);
+                return;
+              }
 
-            if (!userData) {
-              handleBadRequest(sendResponse, strings.errors.invalidJSON);
-              return;
-            }
-
-            handlePutRequest(sendResponse, userId as UUID, userData);
-          });
-        } else if (method === HttpMethod.DELETE) {
-          handleDeleteRequest(sendResponse, userId as UUID);
-        } else {
-          handleInvalidMethod(sendResponse);
-        }
-        break;
-      default:
-        handleNotFound(sendResponse, 'Invalid endpoint. Try to use /api/users');
-        break;
+              handlePutRequest(sendResponse, userId as UUID, userData);
+            });
+          } else if (method === HttpMethod.DELETE) {
+            handleDeleteRequest(sendResponse, userId as UUID);
+          } else {
+            handleInvalidMethod(sendResponse);
+          }
+          break;
+        default:
+          handleNotFound(sendResponse, 'Invalid endpoint. Try to use /api/users');
+          break;
+      }
+    } catch {
+      handleServerError(sendResponse);
     }
   });
 
